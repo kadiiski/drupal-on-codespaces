85eb93283143675ee9952dd7147cad2bd979518c
Ivaylo Kadiyski <kadiiski94@gmail.com>
2023-08-23 08:44:07 +0000

    updating setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 composer create-project drupal/recommended-project drupal_site
 cd drupal_site
 composer require drush/drush
-vendor/bin/drush si --db-url=sqlite://drupal:drupal@127.0.0.1:3306/drupal
+vendor/bin/drush si --db-url=sqlite://drupal:drupal@127.0.0.1:3306/drupal --yes
 vendor/bin/drush upwd admin 123
-vendor/bin/drush generate phpstorm-metadata
-
+chmod 777 -R .
 echo '$settings["reverse_proxy"] = TRUE;' >> web/sites/default/settings.php
 echo '$settings["reverse_proxy_addresses"] = ["127.0.0.1"];' >> web/sites/default/settings.php
 echo 'export PATH="./vendor/bin:$PATH"' >> ~/.bashrc
-export PATH="./vendor/bin:$PATH
+export PATH="./vendor/bin:$PATH"
